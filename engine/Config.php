441046d286c944fb1695da1ab5035d676ee99dd7
RomainDW <romain.ollier34@gmail.com>
2018-11-23 17:09:14 +0100

    :heavy_plus_sign: Config & Database

---
+++ engine/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Engine;
+
+final class Config
+{
+    // Identifiants Database
+    const
+        DB_HOST = 'localhost',
+        DB_NAME = 'blog-php-p5',
+        DB_USR = 'root',
+        DB_PWD = '',
+        // Titre du site
+        SITE_NAME = 'Blog de Romain Ollier';
+}
