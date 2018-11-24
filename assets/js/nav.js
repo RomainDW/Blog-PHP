10f926e435333e9c24546a0ab70c5ee58143b6a6
RomainDW <romain.ollier34@gmail.com>
2018-11-24 17:48:16 +0100

    :recycle: js organisation

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -22,6 +22,20 @@
         offset: 80
     });
 
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 70)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
     // Collapse Navbar
     var navbarCollapse = function() {
         if ($("#mainNav").offset().top > 100) {
