da78e01b35134b1e760077e397d16776a7eae107
RomainDW <romain.ollier34@gmail.com>
2018-11-24 16:27:04 +0100

    :heavy_plus_sign: adding Blog Controller

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Romain
- * Date: 24/11/2018
- * Time: 15:33
- */
 
 namespace Controllers;
 
 
-class BlogController
+class BlogController extends Controller
 {
+    public function index() {
 
+        echo $this->twig->render('blog/blog.html.twig');
+    }
 }
