3d08098f56a2a9b1f42cd2981d1e50acbb9cc952
RomainDW <romain.ollier34@gmail.com>
2018-11-28 20:30:05 +0100

    :heavy_plus_sign: update post

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -72,4 +72,71 @@ class BlogController extends Controller
             'message'   => $message,
         ]);
     }
+
+    public function update() {
+        if (isset($_GET['id'])) {
+
+            $post = $this->blogModel->getPostById($_GET['id']);
+
+            if ($post) {
+
+                $message = '';
+
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                    if ($_POST['active'] == null) {
+                        $_POST['active'] = 0;
+                    }
+
+                    if (isset($_FILES['image']) AND $_FILES['image']['error'] == 0) {
+
+
+                        if ($_FILES['image']['size'] <= 1000000) {
+
+                            $infoImage = pathinfo($_FILES['image']['name']);
+                            $imageExtension = $infoImage['extension'];
+                            $allowedExtensions = array('jpg', 'jpeg', 'gif', 'png');
+
+                            if (in_array($imageExtension, $allowedExtensions)) {
+                                if (file_exists('uploads' . $post['image'])){
+                                    unlink('uploads/' . $post['image']);
+                                }
+                                move_uploaded_file($_FILES['image']['tmp_name'],  'uploads/' . basename($_FILES['image']['name']));
+                            }
+                        }
+
+                        $image = basename($_FILES['image']['name']);
+
+                    } else {
+                        $image = null;
+                    }
+
+                    $data = [
+                        'id'            => $_GET['id'],
+                        'title'         => $_POST['title'],
+                        'subtitle'      => $_POST['subtitle'],
+                        'content'       => $_POST['content'],
+                        'image'         => $image,
+                        'active'        => $_POST['active']
+                    ];
+
+                    if ($this->blogModel->updatePost($data)) {
+                        $message = 'L\'article a bien été modifié !';
+                    }
+
+                    $post = $this->blogModel->getPostById($_GET['id']);
+
+                }
+
+                echo $this->twig->render('admin/blog/update.html.twig', [
+                    'post'      => $post,
+                    'message'   => $message
+                ]);
+            } else {
+                $this->redirect404();
+            }
+        } else {
+            $this->redirect404();
+        }
+    }
 }
--- models/Blog.php
+++ models/Blog.php
@@ -14,5 +14,21 @@ class Blog extends Model
         return $req->execute($data);
     }
 
+    public function getPostById($id) {
+        $req = $this->db->prepare('SELECT * from posts WHERE id = ' . $id);
+        $req->execute();
+        return $req->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function updatePost($data) {
+        $req = $this->db->prepare('UPDATE posts SET title = :title, subtitle = :subtitle, content = :content, image = :image, active = :active WHERE id = :id LIMIT 1');
+        $req->bindValue(':id', $data['id'], \PDO::PARAM_INT);
+        $req->bindValue(':title', $data['title'], \PDO::PARAM_STR);
+        $req->bindValue(':subtitle', $data['subtitle'], \PDO::PARAM_STR);
+        $req->bindValue(':content', $data['content'], \PDO::PARAM_STR);
+        $req->bindValue(':image', $data['image'], \PDO::PARAM_STR);
+        $req->bindValue(':active', $data['active'], \PDO::PARAM_BOOL);
+        return $req->execute();
+    }
 
 }
