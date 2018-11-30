07dd755557a452090150890a0e121c58e0305b0d
Romain Ollier <romain.ollier34@gmail.com>
2018-11-30 19:45:38 +0100

    :wrench: fix verification in show() method

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -149,9 +149,7 @@ class BlogController extends Controller
 
     public function show() {
 
-        $post = $this->blogModel->getPostById($_GET['id']);
-
-        if (isset($_GET['id']) && $post) {
+        if (isset($_GET['id']) && $post = $this->blogModel->getPostById($_GET['id'])) {
 
             echo $this->twig->render('front/blog/post.html.twig', [
                 'post' => $post
