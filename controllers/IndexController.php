e3d03c14e6bb89da3e52f9d19b160ec5e64b85a5
Romain Ollier <romain.ollier34@gmail.com>
2018-12-05 19:10:25 +0100

    :arrow_up: Move the contact method into IndexController.php

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -10,4 +10,29 @@ class IndexController extends Controller
         echo $this->twig->render('front/index/home.html.twig');
     }
 
+    public function contact() {
+
+        // Check for empty fields
+        if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            http_response_code(500);
+            exit();
+        }
+
+        $name = strip_tags(htmlspecialchars($_POST['name']));
+        $email = strip_tags(htmlspecialchars($_POST['email']));
+        $phone = strip_tags(htmlspecialchars($_POST['phone']));
+        $message = strip_tags(htmlspecialchars($_POST['message']));
+
+        // Create the email and send the message
+        $to = "contact@romain-ollier.com";
+        $subject = "Message via formulaire de contact:  $name";
+        $body = "Vous avez reçu un nouveau message de votre formulaire de contact de site Web.\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email\n\nTel: $phone\n\nMessage:\n$message";
+        $header = "From: noreply@romain-ollier.com\n";
+        $header .= "Reply-To: $email";
+
+        if(!mail($to, $subject, $body, $header))
+            http_response_code(500);
+
+    }
+
 }
