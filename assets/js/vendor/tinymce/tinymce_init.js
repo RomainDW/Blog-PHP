06005dec4627905acdacface162140a5a54f46d6
Romain Ollier <romain.ollier34@gmail.com>
2018-12-07 09:40:47 +0100

    :heavy_plus_sign: Adding TinyMCE + Filemanager

---
+++ assets/js/vendor/tinymce/tinymce_init.js
@@ -0,0 +1,20 @@
+tinymce.init({
+    selector: '.article-content',
+    height: 500,
+    menubar: false,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor textcolor',
+        'searchreplace visualblocks responsivefilemanager code',
+        'insertdatetime media table contextmenu paste code help wordcount'
+    ],
+    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code',
+    content_css: [
+        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+        '//www.tinymce.com/css/codepen.min.css'],
+    image_advtab: true ,
+    image_caption: true,
+
+    external_filemanager_path:"/assets/filemanager/",
+    filemanager_title:"Responsive Filemanager" ,
+    external_plugins: { "filemanager" : "/assets/filemanager/plugin.min.js"}
+});
