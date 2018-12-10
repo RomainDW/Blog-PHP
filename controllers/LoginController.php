df742208a3693630af763806803ef799273d4e6a
Romain Ollier <romain.ollier34@gmail.com>
2018-12-10 21:33:38 +0100

    :heavy_plus_sign: [Login Controller] : adding registration method

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -49,6 +49,55 @@ class LoginController extends Controller
         ]);
     }
 
+    public function registration() {
+
+        $message = [
+            'success'   => null,
+            'error'     => null
+        ];
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $email = htmlspecialchars($_POST['email']);
+            $name = htmlspecialchars($_POST['name']);
+            $password = htmlspecialchars($_POST['password']);
+            $passwordCheck = htmlspecialchars($_POST['passwordCheck']);
+            $userExist = $this->usersModel->checkUserByEmail($email);
+
+            if (empty($email) || empty($name) || empty($password) || empty($passwordCheck)) {
+
+                $message['error'] = "Tous les champs n'ont pas été remplis";
+
+            } elseif ($password != $passwordCheck) {
+
+                $message['error'] = "Les mots de passe ne correspondent pas";
+
+            } elseif ($userExist) {
+                $message['error'] = "Cet utilisateur existe déjà";
+            } else {
+
+                $data = [
+                    'name'      => $_POST['name'],
+                    'email'     => $_POST['email'],
+                    'password'  => sha1($_POST['password']),
+                    'role'      => 0
+                ];
+
+                if ($this->usersModel->setUser($data)) {
+                    //TODO: redirect to "my account"
+                    $message['success'] = 'Compte créé';
+                } else {
+                    $message['error'] = "Une erreur s'est produite";
+                }
+
+            }
+        }
+
+        echo $this->twig->render('front/login/registration.html.twig', [
+            'message'   => $message
+        ]);
+    }
+
     public function logout() {
 
         if ($this->isLogged()) {
