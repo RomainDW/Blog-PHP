acf3e644a88e3245885c3084931e4817ce107006
Romain Ollier <romain.ollier34@gmail.com>
2018-12-10 21:32:55 +0100

    :heavy_plus_sign: [User Model] : adding checkUserByEmail & SetUser

--- models/Users.php
+++ models/Users.php
@@ -9,7 +9,7 @@ class Users extends Model
 
         $data = [
             'email'     => $email,
-            'password'  => $password
+            'password'  => sha1($password)
         ];
 
         $req = $this->db->prepare('SELECT * FROM users WHERE email = :email AND password = :password');
@@ -18,5 +18,21 @@ class Users extends Model
         $req->execute();
 
         return $req->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function checkUserByEmail ($email) {
+        $req = $this->db->prepare('SELECT * FROM users WHERE email = :email LIMIT 1');
+        $req->bindValue(':email', $email, \PDO::PARAM_STR);
+        $req->execute();
+        return $req->rowCount();
+    }
+
+    public function setUser($data) {
+        $req = $this->db->prepare('INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)');
+        $req->bindValue(':name', $data['name'], \PDO::PARAM_STR);
+        $req->bindValue(':email', $data['email'], \PDO::PARAM_STR);
+        $req->bindValue(':password', $data['password'], \PDO::PARAM_STR);
+        $req->bindValue(':role', $data['role'], \PDO::PARAM_BOOL);
+        return $req->execute();
     }
 }
